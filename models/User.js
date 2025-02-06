6ac6afa033a54c6cb535de9537ae5e446ef58e67
NayakYadav1 <yadavnayak202@gmail.com>
2025-02-06 20:22:36 +0545

    Model for the user

--- models/User.js
+++ models/User.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, select: false },
-    phone: { type: String, required: true, maxlength: 30 },
-    address: { type: String, required: true },
-    role: { type: String, enum: ['Admin', 'Staff'], default: 'Staff' },
-    status: { type: Boolean, default: true },
-    resetPasswordToken: String,
-    resetPasswordExpires: Date,
-}, { timestamps: true });
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    address: String,
+    phone: String,
+    profilePic: String,
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
